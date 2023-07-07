462fd6fd26782d40ee6475e97699e1037b4ec8e8
Nazly Zambrano <nazmi.016@gmail.com>
2023-07-07 09:03:49 -0500

    update webpack menu 375px

---
+++ src/app/index.js
@@ -0,0 +1,19 @@
+import "./styles/style.scss";
+import "@shoelace-style/shoelace/dist/themes/light.css";
+import "@shoelace-style/shoelace";
+
+const menuIcon = document.getElementById("menuIcon");
+const navBar = document.querySelector(".navBar");
+
+document.addEventListener("DOMContentLoaded", () => {
+  // function toggleMenu() {
+  //   navBar.classList.toggle("menu-open");
+  // }
+  // menuIcon.addEventListener("click", toggleMenu);
+  // function handleWindowResize() {
+  //   if (window.innerWidth >= 376) {
+  //     navBar.classList.remove("menu-open");
+  //   }
+  // }
+  // window.addEventListener("resize", handleWindowResize);
+});
